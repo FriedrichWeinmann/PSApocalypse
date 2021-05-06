1db3ce74f9e3661e9fefadfd9c4d95d988865792
Friedrich Weinmann <FriedrichWeinmann@users.noreply.github.com>
2021-05-06 19:57:51 +0200

    Added function

---
+++ PSApocalypse/functions/Invoke-Armaggeddon.ps1
@@ -0,0 +1,31 @@
+﻿function Invoke-Armaggeddon {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+        Author: Friedrich Weinmann
+        Company: Microsoft
+        Version: 1.0.0
+        Created on: 2021-05-06
+    #>
+    [CmdletBinding()]
+    param (
+
+    )
+    begin {
+
+    }
+    process {
+
+    }
+    end {
+
+    }
+}
